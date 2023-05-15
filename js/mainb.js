24d7a7986aa04984efdca8ae04be487573737a73
cocotiger <thwngusl@naver.com>
2023-05-15 16:26:42 +0900

    리코딩 작업

---
+++ js/mainb.js
@@ -0,0 +1,15 @@
+﻿const toggleEl = document.querySelector('.iconbar')
+const leftWrapEl = document.querySelector('#leftWrap')
+const rightWrapEl = document.querySelector('#rightWrap')
+toggleEl.addEventListener('click', () => {
+if (leftWrapEl.classList.contains('show')) {
+		leftWrapEl.classList.remove('show')
+		rightWrapEl.classList.remove('show')
+		toggleEl.classList.remove('show')
+	}
+	else {
+		leftWrapEl.classList.add('show')
+		rightWrapEl.classList.add('show')
+		toggleEl.classList.add('show')
+	}
+})
